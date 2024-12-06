b3b8a4bdd6c22bb5954a2eae6df947cd621dbfd6
Kyle Wenholz <kyle@krwenholk.com>
2024-12-06 18:35:53 +0000

    Add final output to newDay template

--- src/newDay.ts
+++ src/newDay.ts
@@ -7,21 +7,27 @@ import * as fs from "fs";
 import { logger } from "./logger";
 
 export function partOne(filePath: string): void {
-    const fileContents = fs.readFileSync(filePath, "utf-8");
-    let lines = fileContents.split("\\n");
-    const expected = lines[0];
-    lines = lines.slice(1);
-    logger.info(\`Running day ${dayNumber} part one with \${lines.length} lines and expected \${expected}\`);
-    // TODO: Implement part one logic
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  let lines = fileContents.split("\\n");
+  const expected = lines[0];
+  lines = lines.slice(1);
+  logger.info(\`Running day ${dayNumber} part one with \${lines.length} lines and expected \${expected}\`);
+
+  // TODO: Implement part one logic
+
+  logger.info({ value: "", expected: expected }, "Day ${dayNumber} part one");
 }
 
 export function partTwo(filePath: string): void {
-    const fileContents = fs.readFileSync(filePath, "utf-8");
-    let lines = fileContents.split("\\n");
-    const expected = lines[0];
-    lines = lines.slice(1);
-    logger.info(\`Running day ${dayNumber} part two with \${lines.length} lines\ and expected \${expected}\`);
-    // TODO: Implement part two logic
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  let lines = fileContents.split("\\n");
+  const expected = lines[0];
+  lines = lines.slice(1);
+  logger.info(\`Running day ${dayNumber} part two with \${lines.length} lines\ and expected \${expected}\`);
+
+  // TODO: Implement part two logic
+
+  logger.info({ value: "", expected: expected }, "Day ${dayNumber} part two");
 }
 `;
 
