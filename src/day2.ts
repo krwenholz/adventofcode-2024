be9d036179298f877b5b997be4a1ee8dd9bda87a
Kyle Wenholz <kyle@krwenholk.com>
2024-12-09 17:25:30 +0000

    [day2 part2] Finish catching edge cases!

--- src/day2.ts
+++ src/day2.ts
@@ -39,6 +39,8 @@ export function partOne(filePath: string): void {
   logger.info({ value: safeCount, expected: expected }, "Day 2 part one");
 }
 
+const allowedDiffs = new Set([1, 2, 3]);
+
 function reportIsSafe(report: number[]): [boolean, number] {
   let increasing = report[0] < report[1];
   for (let i = 0; i < report.length - 1; i++) {
@@ -47,12 +49,12 @@ function reportIsSafe(report: number[]): [boolean, number] {
 
     if (increasing) {
       let diff = next - cur;
-      if (cur < next && [1, 2, 3].includes(diff)) {
+      if (cur < next && allowedDiffs.has(diff)) {
         continue;
       }
     } else {
       let diff = cur - next;
-      if (cur > next && [1, 2, 3].includes(diff)) {
+      if (cur > next && allowedDiffs.has(diff)) {
         continue;
       }
     }
@@ -66,24 +68,77 @@ function reportIsSafe(report: number[]): [boolean, number] {
 function reportLineIsSafe(l: string): boolean {
   let levels = l.split(" ").map(Number);
 
-  let [safe, skippedLevel] = reportIsSafe(levels);
+  // Start with an easy filter or two to help debug initial incorrect solution
+  let zeroCount = 0;
+  let tooLargeCount = 0;
+  for (let i = 0; i < levels.length - 1; i++) {
+    let diff = Math.abs(levels[i] - levels[i + 1]);
+    if (diff === 0) {
+      zeroCount++;
+    }
+    if (diff > 3) {
+      tooLargeCount++;
+    }
+
+    if (
+      zeroCount >= 2 ||
+      tooLargeCount >= 2 ||
+      (zeroCount > 0 && tooLargeCount > 0)
+    ) {
+      logger.debug(
+        { levels: levels.join(" "), zeroCount, tooLargeCount },
+        "Diff count too large, unsafe"
+      );
+      return false;
+    }
+  }
+
+  let [safe, firstSkippedLevel] = reportIsSafe(levels);
   if (!safe) {
-    let levelsReduced = levels
-      .slice(skippedLevel)
-      .concat(levels.slice(skippedLevel + 1));
+    let levelsReduced0 = levels
+      .slice(0, firstSkippedLevel)
+      .concat(levels.slice(firstSkippedLevel + 1, levels.length));
+    let levelsReduced1 = levels
+      .slice(0, firstSkippedLevel + 1)
+      .concat(levels.slice(firstSkippedLevel + 2, levels.length));
     logger.debug(
-      { levels: levels.join(" "), levelsReduced, skippedLevel },
+      {
+        levels: levels.join(" "),
+        levelsReduced0,
+        levelsReduced1,
+        firstSkippedLevel,
+      },
       "One unsafe"
     );
-    [safe, skippedLevel] = reportIsSafe(levelsReduced);
-    if (!safe) {
-      logger.debug({ levels: levels.join(" "), skippedLevel }, "Second unsafe");
+    let [safe0, secondSkippedLevel0] = reportIsSafe(levelsReduced0);
+    let [safe1, secondSkippedLevel1] = reportIsSafe(levelsReduced1);
+    if (!(safe0 || safe1)) {
+      logger.debug(
+        {
+          levels: levels.join(" "),
+          firstSkippedLevel,
+          secondSkippedLevel0,
+          secondSkippedLevel1,
+        },
+        "Second unsafe"
+      );
+      // I don't love this, but it catches a special case I'm struggling to reason about
+      // more abstractly. It catches a direction issue with the first level.
+      if (firstSkippedLevel === 1) {
+        let [safe2, thirdSkippedLevel] = reportIsSafe(
+          levels.slice(1, levels.length)
+        );
+        if (safe2) {
+          logger.debug({ levels: levels.join(" ") }, "Safe with skipped first");
+          return true;
+        }
+      }
       return false;
     }
     return true;
   }
 
-  logger.debug({ l, skippedLevel }, "Safe");
+  logger.trace({ l, skippedLevel: firstSkippedLevel }, "Safe");
   return true;
 }
 
