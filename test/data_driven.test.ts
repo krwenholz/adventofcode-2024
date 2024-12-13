63c0d4d9af7c22c6675fadf72858c8d15f950494
Kyle Wenholz <kyle@krwenholz.com>
2024-12-13 18:04:24 +0000

    Tidy test names

--- test/data_driven.test.ts
+++ test/data_driven.test.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 fs.readdirSync("./inputs").forEach((file) => {
     const day = file.match(/day(\d+)/)?.[1];
     const name = file.match(/day\d+\.(\w+)\.txt/)?.[1];
-    describe("data driven day tests", () => {
+    describe(`data driven day ${day}:${name}`, () => {
         if (!name) {
             test(`skipping ${file}`, () => { });
             return;
@@ -15,14 +15,14 @@ fs.readdirSync("./inputs").forEach((file) => {
         const partTwoExpected = contents[1];
 
         if (partOneExpected != "???") {
-            test(`testing day ${day} part one with case ${name}`, async () => {
+            test(`part one`, async () => {
                 const module = await import(`../src/day${day}.ts`);
                 expect(module.partOne("./inputs/" + file)).toBe(Number(partOneExpected));
             });
         }
 
         if (partTwoExpected != "???") {
-            test(`testing day ${day} part two with case ${name}`, async () => {
+            test(`part two`, async () => {
                 const module = await import(`../src/day${day}.ts`);
                 expect(module.partTwo("./inputs/" + file)).toBe(Number(partTwoExpected));
             });
