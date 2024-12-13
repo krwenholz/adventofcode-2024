6a45319eb0cd449765a2c0d606ce467bdb35a40d
Kyle Wenholz <kyle@krwenholk.com>
2024-12-13 18:27:02 +0000

    [day5 part1] Done

---
+++ src/day5.ts
@@ -0,0 +1,100 @@
+import * as fs from "fs";
+import { logger } from "./logger";
+import { kMaxLength } from "buffer";
+
+type Rule = {
+  before: number;
+  after: number;
+};
+
+function isValidUpdate(pages: number[], rules: Map<number, Rule[]>): boolean {
+  if (pages[0] === 61) {
+    logger.debug({ pages: pages, 29: rules.get(29) }, "Checking page");
+  }
+  const seenSoFar = new Set<number>();
+  for (let page of pages) {
+    const pageRules = rules.get(page);
+    if (!pageRules) {
+      continue;
+    }
+
+    for (let rule of pageRules) {
+      for (let seen of seenSoFar) {
+        if (rule.before === page && rule.after === seen) {
+          return false;
+        }
+      }
+      seenSoFar.add(page);
+    }
+  }
+
+  return true;
+}
+
+export function partOne(filePath: string): number {
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  let lines = fileContents.split("\n");
+  const expected = lines[0];
+  lines = lines.slice(2);
+  logger.info(
+    `Running day 5 part one with ${lines.length} lines and expected ${expected}`
+  );
+
+  const splitIdx = lines.findIndex((line) => {
+    if (!line) {
+      return true;
+    }
+  });
+
+  let rules = new Map<number, Rule[]>();
+  lines.slice(0, splitIdx).forEach((line) => {
+    let [beforeTxt, afterTxt] = line.split("|");
+    const before = parseInt(beforeTxt);
+    const after = parseInt(afterTxt);
+
+    if (!rules.has(before)) {
+      rules.set(before, []);
+    }
+    rules.get(before)?.push({ before: before, after: after });
+
+    if (!rules.has(after)) {
+      rules.set(after, []);
+    }
+    rules.get(after)?.push({ before: before, after: after });
+  });
+
+  const updates = lines.slice(splitIdx + 1).map((line) => {
+    return line.split(",").map((page) => parseInt(page));
+  });
+
+  const medians: number[] = [0];
+  for (let update of updates) {
+    if (isValidUpdate(update, rules)) {
+      medians.push(update[Math.floor(update.length / 2)]);
+      logger.debug(
+        { update: update, median: update[Math.floor(update.length / 2)] },
+        "Valid update"
+      );
+    }
+  }
+
+  const answer = medians.reduce((acc, val) => acc + val, 0);
+
+  logger.info({ value: answer, expected: expected }, "Day 5 part one");
+  return answer;
+}
+
+export function partTwo(filePath: string): number {
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  let lines = fileContents.split("\n");
+  const expected = lines[1];
+  lines = lines.slice(2);
+  logger.info(
+    `Running day 5 part two with ${lines.length} lines and expected ${expected}`
+  );
+
+  // TODO: Implement part two logic
+
+  logger.info({ value: "", expected: expected }, "Day 5 part two");
+  return NaN;
+}
