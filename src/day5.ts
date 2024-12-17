05a8f54a5d155285fd5a0d16158fd43bdac6eb3a
Kyle Wenholz <kyle@krwenholk.com>
2024-12-17 22:23:01 +0000

    [day5 part2] Done

--- src/day5.ts
+++ src/day5.ts
@@ -7,9 +7,6 @@ type Rule = {
 };
 
 function isValidUpdate(pages: number[], rules: Map<number, Rule[]>): boolean {
-  if (pages[0] === 61) {
-    logger.debug({ pages: pages, 29: rules.get(29) }, 'Checking page');
-  }
   const seenSoFar = new Set<number>();
   for (const page of pages) {
     const pageRules = rules.get(page);
@@ -83,6 +80,35 @@ export function partOne(filePath: string): number {
   return answer;
 }
 
+function tryFixing(
+  update: number[],
+  rules: Map<number, Rule[]>,
+): [number[], boolean] {
+  // the number seen to it's position
+  const seenSoFar = new Map<number, number>();
+  for (let i = 0; i < update.length; i++) {
+    const page = update[i];
+    const pageRules = rules.get(page);
+    if (!pageRules) {
+      continue;
+    }
+
+    for (const rule of pageRules) {
+      for (const [seen, idx] of seenSoFar.entries()) {
+        if (rule.before === page && rule.after === seen) {
+          // swap the two
+          const temp = update[i];
+          update[i] = update[idx];
+          update[idx] = temp;
+          return [update, true];
+        }
+      }
+      seenSoFar.set(page, i);
+    }
+  }
+  return [update, false];
+}
+
 export function partTwo(filePath: string): number {
   const fileContents = fs.readFileSync(filePath, 'utf-8');
   let lines = fileContents.split('\n');
@@ -92,8 +118,51 @@ export function partTwo(filePath: string): number {
     `Running day 5 part two with ${lines.length} lines and expected ${expected}`,
   );
 
-  // TODO: Implement part two logic
+  const splitIdx = lines.findIndex(line => {
+    if (!line) {
+      return true;
+    }
+  });
 
-  logger.info({ value: '', expected: expected }, 'Day 5 part two');
-  return NaN;
+  const rules = new Map<number, Rule[]>();
+  lines.slice(0, splitIdx).forEach(line => {
+    const [beforeTxt, afterTxt] = line.split('|');
+    const before = parseInt(beforeTxt);
+    const after = parseInt(afterTxt);
+
+    if (!rules.has(before)) {
+      rules.set(before, []);
+    }
+    rules.get(before)?.push({ before: before, after: after });
+
+    if (!rules.has(after)) {
+      rules.set(after, []);
+    }
+    rules.get(after)?.push({ before: before, after: after });
+  });
+
+  const updates = lines.slice(splitIdx + 1).map(line => {
+    return line.split(',').map(page => parseInt(page));
+  });
+
+  const medians: number[] = [];
+  for (let update of updates) {
+    let valid = isValidUpdate(update, rules);
+    while (!valid) {
+      // Could do a topological sort: https://www.geeksforgeeks.org/topological-sorting/
+      // But we'll just try fixing it instead
+      let adjusted = false;
+      [update, adjusted] = tryFixing(update, rules);
+      if (!adjusted) {
+        logger.debug({ update: update }, 'Fixed update');
+        medians.push(update[Math.floor(update.length / 2)]);
+        valid = true;
+      }
+    }
+  }
+
+  const answer = medians.reduce((acc, val) => acc + val, 0);
+
+  logger.info({ value: answer, expected: expected }, 'Day 5 part two');
+  return answer;
 }
