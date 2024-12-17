02f35579bc56306abda6df616a316f984c9a3458
Kyle Wenholz <kyle@krwenholz.com>
2024-12-17 16:20:03 +0000

    [day7 part2] Done

--- src/day7.ts
+++ src/day7.ts
@@ -10,9 +10,10 @@ function canEval(val: number, nums: number[], acc: number): boolean {
     return false;
   }
 
+  const nextNums = nums.slice(1);
   return (
-    canEval(val, nums.slice(1), acc + nums[0]) ||
-    canEval(val, nums.slice(1), acc * nums[0])
+    canEval(val, nextNums, acc + nums[0]) ||
+    canEval(val, nextNums, acc * nums[0])
   );
 }
 
@@ -39,6 +40,25 @@ export function partOne(filePath: string): number {
   return sum;
 }
 
+function canEvalTwo(val: number, nums: number[], acc: number): boolean {
+  if (nums.length === 0) {
+    return val === acc;
+  }
+
+  if (val < acc) {
+    return false;
+  }
+
+  const num = nums[0];
+  const numDigits = (Math.log(num) * Math.LOG10E + 1) | 0;
+  const nextNums = nums.slice(1);
+  return (
+    canEvalTwo(val, nextNums, acc + num) ||
+    canEvalTwo(val, nextNums, acc * num) ||
+    canEvalTwo(val, nextNums, acc * 10 ** numDigits + num)
+  );
+}
+
 export function partTwo(filePath: string): number {
   const fileContents = fs.readFileSync(filePath, 'utf-8');
   let lines = fileContents.split('\n');
@@ -48,8 +68,16 @@ export function partTwo(filePath: string): number {
     `Running day 7 part two with ${lines.length} lines and expected ${expected}`,
   );
 
-  // TODO: Implement part two logic
+  let sum = 0;
+  for (let l of lines) {
+    let [valStr, numsStr] = l.split(': ');
+    const val = Number(valStr);
+    const nums = numsStr.split(' ').map(Number);
+    if (canEvalTwo(val, nums.slice(1), nums[0])) {
+      sum += val;
+    }
+  }
 
-  logger.info({ value: '', expected: expected }, 'Day 7 part two');
-  return NaN;
+  logger.info({ value: sum, expected: expected }, 'Day 7 part two');
+  return sum;
 }
