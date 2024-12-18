60117b2c8eba24650e268ad95a0b49d6d12f9c7f
Kyle Wenholz <kyle@krwenholz.com>
2024-12-18 17:23:27 +0000

    [day9 part2] Done

--- src/day9.ts
+++ src/day9.ts
@@ -83,6 +83,22 @@ export function partOne(filePath: string): number {
   return checksum;
 }
 
+type Slot = {
+  kind: 'space' | 'data';
+  size: number;
+  dataId: number;
+};
+
+function slotsToString2(slots: Slot[]): string {
+  return slots
+    .map(slot => {
+      return slot.kind === 'space'
+        ? '.'.repeat(slot.size)
+        : `${slot.dataId}`.repeat(slot.size);
+    })
+    .join('');
+}
+
 export function partTwo(filePath: string): number {
   const fileContents = fs.readFileSync(filePath, 'utf-8');
   let lines = fileContents.split('\n');
@@ -92,8 +108,110 @@ export function partTwo(filePath: string): number {
     `Running day 9 part two with ${lines.length} lines and expected ${expected}`,
   );
 
-  // TODO: Implement part two logic
+  const disk = lines[0];
+  let slots: Slot[] = [];
+  let idx = 0;
+  for (let i = 0; i < disk.length; i++) {
+    const val = parseInt(disk[i]);
+    if (i % 2 === 0) {
+      // it's data
+      const dataId = i / 2;
+      slots.push({ kind: 'data', size: val, dataId: dataId });
+    } else {
+      // it's space
+      slots.push({ kind: 'space', size: val, dataId: -1 });
+    }
+    idx += val;
+  }
+  logger.debug(
+    {
+      slots: slotsToString2(slots),
+    },
+    'Slots pre-compaction',
+  );
 
-  logger.info({ value: '', expected: expected }, 'Day 9 part two');
-  return NaN;
+  let dataSlot = slots.findLastIndex(slot => slot.kind === 'data');
+  while (dataSlot > 0) {
+    let data = slots[dataSlot];
+
+    // find the next space with enough room for this data
+    let spaceSlot = slots.findIndex(slot => slot.kind === 'space');
+    let space = slots[spaceSlot];
+    for (; spaceSlot < slots.length; spaceSlot++) {
+      if (slots[spaceSlot].kind !== 'space') {
+        continue;
+      }
+
+      space = slots[spaceSlot];
+      if (data.size <= space.size) {
+        break;
+      }
+    }
+
+    if (spaceSlot === slots.length) {
+      // no space for this data, move along to the next data slot
+      for (dataSlot--; dataSlot > 0; dataSlot--) {
+        if (slots[dataSlot].kind === 'data') {
+          break;
+        }
+      }
+      // assume we found the next data and restart our loop
+      continue;
+    }
+
+    // We didn't find a good space or data slot, move along
+    if (spaceSlot > dataSlot) {
+      for (dataSlot--; dataSlot > 0; dataSlot--) {
+        if (slots[dataSlot].kind === 'data') {
+          break;
+        }
+      }
+      continue;
+    }
+
+    // if we're here, we have a space for the data
+    // move the data to the space
+    logger.debug({ data, dataSlot, space, spaceSlot }, 'Moving data to space');
+    slots[dataSlot] = { kind: 'space', size: data.size, dataId: -1 };
+    slots[spaceSlot] = { kind: 'data', size: data.size, dataId: data.dataId };
+    const remainingSpace = space.size - data.size;
+    if (remainingSpace > 0) {
+      slots = slots
+        .slice(0, spaceSlot + 1)
+        .concat([{ kind: 'space', size: remainingSpace, dataId: -1 }])
+        .concat(slots.slice(spaceSlot + 1));
+    }
+
+    // if we're here, we've moved the data to the space
+    // move to the next data slot
+    for (; dataSlot > 0; dataSlot--) {
+      if (slots[dataSlot].kind === 'data') {
+        break;
+      }
+    }
+    logger.debug({ slots: slotsToString2(slots), dataSlot }, 'Slots post-move');
+  }
+  logger.debug(
+    {
+      slots: slotsToString2(slots),
+    },
+    'Slots post-compaction',
+  );
+
+  let checksum = 0;
+  let pos = 0;
+  for (let slot of slots) {
+    if (slot.kind === 'space') {
+      pos += slot.size;
+      continue;
+    }
+
+    for (let i = 0; i < slot.size; i++) {
+      checksum += pos * slot.dataId;
+      pos++;
+    }
+  }
+
+  logger.info({ value: checksum, expected: expected }, 'Day 9 part two');
+  return checksum;
 }
