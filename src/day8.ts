b6cc5314c61c13657783719df6b1b2ab9989be23
Kyle Wenholz <kyle@krwenholz.com>
2024-12-18 01:03:11 +0000

    [day8 part1] Done

--- src/day8.ts
+++ src/day8.ts
@@ -1,6 +1,14 @@
 import * as fs from 'fs';
 import { logger } from './logger';
 
+type Station = {
+  frequency: string;
+  row: number;
+  col: number;
+};
+
+const validFrequencies = new RegExp('^[A-Za-z0-9]$');
+
 export function partOne(filePath: string): number {
   const fileContents = fs.readFileSync(filePath, 'utf-8');
   let lines = fileContents.split('\n');
@@ -10,10 +18,56 @@ export function partOne(filePath: string): number {
     `Running day 8 part one with ${lines.length} lines and expected ${expected}`,
   );
 
-  // TODO: Implement part one logic
+  const rows = lines.length;
+  const cols = lines[0].length;
 
-  logger.info({ value: '', expected: expected }, 'Day 8 part one');
-  return NaN;
+  const stations = new Map<string, Station[]>();
+  const antinodes = new Set<string>();
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const frequency = lines[row][col];
+      if (!validFrequencies.test(frequency)) {
+        continue;
+      }
+
+      const curStation = { frequency, row, col };
+      if (!stations.has(curStation.frequency)) {
+        stations.set(curStation.frequency, []);
+      }
+
+      for (let earlierStation of stations.get(curStation.frequency)!) {
+        // We know the earlier station has lower row and col values, so we can subtract the dx from the
+        // earlier station and add to this station
+        const rowDx = curStation.row - earlierStation.row;
+        const colDx = curStation.col - earlierStation.col;
+        const leftRow = earlierStation.row - rowDx;
+        const leftCol = earlierStation.col - colDx;
+        if (leftRow >= 0 && leftRow < rows && leftCol >= 0 && leftCol < cols) {
+          antinodes.add(`${leftRow},${leftCol}`);
+        }
+
+        const rightRow = curStation.row + rowDx;
+        const rightCol = curStation.col + colDx;
+        if (
+          rightRow >= 0 &&
+          rightRow < rows &&
+          rightCol >= 0 &&
+          rightCol < cols
+        ) {
+          antinodes.add(`${rightRow},${rightCol}`);
+        }
+      }
+
+      stations.get(curStation.frequency)?.push(curStation);
+    }
+  }
+
+  logger.info(
+    { locations: antinodes.size, expected: expected },
+    'Day 8 part one',
+  );
+  return antinodes.size;
 }
 
 export function partTwo(filePath: string): number {
