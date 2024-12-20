bef20a1b826ec7422620e0dfbc7aa3070bf1b9eb
Kyle Wenholz <kyle@krwenholk.com>
2024-12-20 01:22:20 +0000

    Much simpler skipping of non-tests

--- test/data_driven.test.ts
+++ test/data_driven.test.ts
@@ -4,19 +4,18 @@ import fs from 'fs';
 fs.readdirSync('./inputs').forEach(file => {
   const day = file.match(/day(\d+)/)?.[1];
   const name = file.match(/day\d+\.(\w+)\.txt/)?.[1];
+  const contents = fs
+    .readFileSync('./inputs/' + file)
+    .toString()
+    .split('\n');
+  const partOneExpected = contents[0];
+  const partTwoExpected = contents[1];
+
+  if (!name || partOneExpected === '???' || partTwoExpected === '???') {
+    return;
+  }
+
   describe(`data_driven ${day}.${name}`, () => {
-    if (!name) {
-      test(`skipping ${file}`, () => {});
-      return;
-    }
-
-    const contents = fs
-      .readFileSync('./inputs/' + file)
-      .toString()
-      .split('\n');
-    const partOneExpected = contents[0];
-    const partTwoExpected = contents[1];
-
     if (partOneExpected != '???') {
       test(`part one`, async () => {
         const module = await import(`../src/day${day}.ts`);
